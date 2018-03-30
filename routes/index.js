b25f1687c6e4f0fd47e1f446ce5195c72ed52182
PeachIcetea29 <ijn29@naver.com>
2018-03-30 20:51:35 +0000

    test bigquery

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,43 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Imports the Google Cloud client library
+const BigQuery = require('@google-cloud/bigquery');
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+ const projectId = "gcp-hackathon18-icn-2906";
+ const sqlQuery = `SELECT category 
+FROM \`bigquery-public-data.san_francisco_311.311_service_requests\` 
+WHERE category like '%General%' 
+GROUP BY category 
+LIMIT 10`;
+
+// Creates a client
+const bigquery = new BigQuery({
+  projectId: projectId,
+});
+
+// Query options list: https://cloud.google.com/bigquery/docs/reference/v2/jobs/query
+const options = {
+  query: sqlQuery,
+  timeoutMs: 10000, // Time out after 10 seconds.
+  useLegacySql: false, // Use standard SQL syntax for queries.
+};
+
+const GOOGLE_APPLICATION_CREDENTIALS = "../GCP Hackathon Korea 2906-4c296f469273.json";
+
+// Runs the query
+bigquery
+  .query(options)
+  .then(results => {
+    const rows = results[0];
+    console.log('Rows:');
+    rows.forEach(row => console.log(row));
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
+
 module.exports = router;
